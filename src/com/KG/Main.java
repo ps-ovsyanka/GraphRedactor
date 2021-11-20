bb68fae5340130f6146d271db6a5b57a697c6641
a.stepanova <ps.ovsyanka@gmail.com>
2021-11-20 06:29:16 +0500

    Перемещение, изменение и создание в одном окне. Координаты курсора и уравнение линии.

--- src/com/KG/Main.java
+++ src/com/KG/Main.java
@@ -2,11 +2,11 @@ package com.KG;
 
 import javax.swing.*;
 import java.awt.*;
-
 public class Main {
 
     public static void main(String[] args) {
         MainFrame frame = new MainFrame();
+
 
     }
 }
